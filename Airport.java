3f7ab0ba46b6a8d7bb7278c44aca737cf64cda9f
Mandy-Hsu <35870216+Mandy-Hsu@users.noreply.github.com>
2020-02-02 22:45:23 -0500

    Add files via upload

---
+++ Airport.java
@@ -0,0 +1,24 @@
+
+public class Airport {
+	
+	private int xcoord;
+	
+	private int ycoord;
+	
+	private int airportfees;
+	
+	public Airport(int xcoord, int ycoord, int airportfees)
+	{
+		this.xcoord = xcoord;
+		this.ycoord = ycoord;
+		this.airportfees = airportfees;
+	}
+	
+	public int getFees() 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
